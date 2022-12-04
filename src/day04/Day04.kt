a5a95a44b6aa97703945a9a821fb9cb6380f643b
Eugen Martynov <eugenemartynov@gmail.com>
2022-12-04 10:12:45 +0100

    Make Day 04

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -2,46 +2,40 @@ package day04
 
 import readInput
 
-private fun Char.toScore() = if (isLowerCase())
-    this - 'a' + 1
-else
-    this - 'A' + 27
-
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return input.map { rugzakContent ->
-            rugzakContent.chunked(size = rugzakContent.length / 2)
-        }.map { compartmentValues ->
-            compartmentValues[0].toSet()
-                .intersect(
-                    compartmentValues[1]
-                        .toSet()
-                ).first()
-        }.sumOf { sameElement ->
-            sameElement.toScore()
+    fun List<String>.toRangeSets() = map { pair ->
+        pair.split(",")
+            .map {
+                it.split("-")
+            }
+    }.map { pairRangeString ->
+        pairRangeString.map { indexValues ->
+            UIntRange(indexValues[0].toUInt(), indexValues[1].toUInt()).toSet()
         }
     }
 
+    fun part1(input: List<String>): Int {
+        return input.toRangeSets().map { pairRange ->
+            val intersect = pairRange[0].intersect(pairRange[1])
+            if (
+                intersect.equals(pairRange[1]) ||
+                intersect.equals(pairRange[0])
+            ) 1 else 0
+        }.sum()
+    }
+
     fun part2(input: List<String>): Int {
-        return input.chunked(3)
-            .map { groupRugzakContent ->
-                groupRugzakContent
-                    .map { it.toSet() }
-                    .reduce { content1, content2 ->
-                        content1.intersect(content2)
-                    }
-            }.map { commonFromGroup ->
-                commonFromGroup.first()
-            }.sumOf { sameElement ->
-                sameElement.toScore()
-            }
+        return input.toRangeSets().map { pairRange ->
+            val intersect = pairRange[0].intersect(pairRange[1])
+            if (intersect.isEmpty()) 0 else 1
+        }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day04/Day04_test")
-    check(part1(testInput) == 157)
-    check(part2(testInput) == 70)
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("day04/Day04")
     println(part1(input))
