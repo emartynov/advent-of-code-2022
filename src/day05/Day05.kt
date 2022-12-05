446ca828d25c61693e4b6cade7977d6b9ba10426
Eugen Martynov <eugenemartynov@gmail.com>
2022-12-05 13:59:28 +0100

    Finish first part

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -2,40 +2,97 @@ package day05
 
 import readInput
 
-fun main() {
+private class Schema(
+    numberOfStacks: Int
+) {
+    private val stacks: MutableList<List<Char>> = MutableList(numberOfStacks) { emptyList() }
+    operator fun get(indexFromOne: Int): List<Char> = stacks[indexFromOne - 1]
 
-    fun List<String>.toRangeSets() = map { pair ->
-        pair.split(",")
-            .map { pairString ->
-                pairString.split("-")
-                    .map { it.toUInt() }
-            }
-    }.map { pairRangeString ->
-        pairRangeString.map { indexValues ->
-            UIntRange(indexValues[0], indexValues[1]).toSet()
-        }
+    fun add(indexFromOne: Int, value: Char): Schema {
+        val updatedStack = listOf(value) + get(indexFromOne)
+        return updateStack(indexFromOne, updatedStack)
     }
 
-    fun part1(input: List<String>): Int {
-        return input.toRangeSets().map { pairRange ->
-            val intersect = pairRange[0].intersect(pairRange[1])
-            if (intersect == pairRange[1] || intersect == pairRange[0]) 1 else 0
-        }.sum()
+    private fun updateStack(indexFromOne: Int, updatedStack: List<Char>): Schema {
+        stacks.add(indexFromOne - 1, updatedStack)
+        stacks.removeAt(indexFromOne)
+        return this
+    }
+
+    fun move(move: Move): Schema {
+        val fromStack = get(move.fromIndex)
+        val toStack = get(move.toIndex)
+
+        val elements = fromStack.subList(fromStack.size - move.numberOfCrates, fromStack.size)
+
+        return updateStack(
+            indexFromOne = move.fromIndex,
+            updatedStack = fromStack.dropLast(move.numberOfCrates)
+        ).updateStack(
+            indexFromOne = move.toIndex,
+            updatedStack = toStack + elements.reversed()
+        )
+    }
+
+    fun  allLast(): String = stacks.map { it.lastOrNull()?.toString().orEmpty() }
+        .joinToString("")
+}
+
+private data class Move(
+    val numberOfCrates: Int,
+    val fromIndex: Int,
+    val toIndex: Int
+)
+
+fun main() {
+
+    fun readStacksAndMoves(input: List<String>): Pair<Schema, List<Move>> {
+        val schemaEndIndex = input.indexOf("")
+        val numberOfStacks = input[schemaEndIndex - 1].chunked(4).last().trim().toInt()
+        var initialSchema = Schema(numberOfStacks)
+        input.subList(0, schemaEndIndex - 1)
+            .map {
+                it.chunked(4)
+                    .forEachIndexed { index, value ->
+                        if (value.startsWith("[")) {
+                            initialSchema = initialSchema.add(index + 1, value[1])
+                        }
+                    }
+            }
+        val moves = input.subList(schemaEndIndex + 1, input.size)
+            .map {
+                it.split(" ")
+            }.map { row ->
+                row.mapNotNull { it.toIntOrNull() }
+            }.map { numbers ->
+                Move(
+                    numberOfCrates = numbers[0],
+                    fromIndex = numbers[1],
+                    toIndex = numbers[2]
+                )
+            }
+        return initialSchema to moves
+    }
+
+    fun playMoves(schema: Schema, moves: List<Move>): Schema =
+        moves.fold(schema) { accSchema, move -> accSchema.move(move) }
+
+    fun part1(input: List<String>): String {
+        val data = readStacksAndMoves(input)
+        val updatedSchema = playMoves(data.first, data.second)
+        return updatedSchema.allLast()
     }
 
     fun part2(input: List<String>): Int {
-        return input.toRangeSets().map { pairRange ->
-            val intersect = pairRange[0].intersect(pairRange[1])
-            if (intersect.isEmpty()) 0 else 1
-        }.sum()
+        return input.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day05/Day05_test")
-    check(part1(testInput) == 2)
-    check(part2(testInput) == 4)
+    check(part1(testInput) == "CMZ")
+//    check(part2(testInput) == 4)
 
     val input = readInput("day05/Day05")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
